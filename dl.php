98baea5d5e926c587d09f0eeedbc3446be2c0b71
Guisch <ghcasier@gmail.com>
2018-08-11 18:14:15 +0200

    update domain

--- dl.php
+++ dl.php
@@ -18,17 +18,17 @@ switch ($_GET['action']) {
 function download($idt) {
     $ygg = new Ygg();
     if ($ygg->login()) {
-    	if ($ygg->download($idt)) {
-        	$location = './dl/download.torrent';
-        
-        	header("Content-Disposition: attachment; filename=" . $idt . ".torrent");
-		header("Content-Type: application/force-download");
-		header("Content-Length: " . filesize($location));
-		header('Content-Transfer-Encoding: binary');
-		header("Connection: close");
-		readfile($location);
-    
-	}
+        if ($ygg->download($idt)) {
+                $location = './dl/download.torrent';
+
+                header("Content-Disposition: attachment; filename=" . $idt . ".torrent");
+                header("Content-Type: application/force-download");
+                header("Content-Length: " . filesize($location));
+                header('Content-Transfer-Encoding: binary');
+                header("Connection: close");
+                readfile($location);
+
+        }
     }
 }
 
